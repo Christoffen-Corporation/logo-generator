c665fc45da4d3dd813e2a7fe45c99e9ee27676e5
M. Gage Morgan <gagemorgan@outlook.com>
2016-05-31 20:39:43 -0400

    UNSTABLE: New CLI features, no more double-click, Makefile fixed, etc. etc.

--- include.h
+++ include.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_H_INCLUDED
 #define INCLUDE_H_INCLUDED
 #include <cairo.h>
+char args[50];
 
 // Declare the colored_triangle() function
 void colored_triangle(int img_width, int img_height, double r, double g, double b, double a, double verticeX1, double verticeY1, double verticeX2, double verticeY2, double verticeX3, double verticeY3, char img_name[50]);
@@ -10,5 +11,17 @@ void outlined_triangle(int img_width, int img_height, double scaleX1, double sca
 
 //Declare the join_triangles() function
 int join_triangles(char tri_one[50], char tri_two[50], char tri_three[50], char tri_four[50], char tri_five[50], char tri_six[50], char tri_seven[50], char img_name[50]); 
+
+void outlined_nologo();
+
+void outlined_all();
+
+void colored_nologo();
+
+void colored_all();
+
+void frankenlogos();
+
+void all_imgs();
 
 #endif
--- main.c
+++ main.c
@@ -1,24 +1,45 @@
+#include <stdio.h>
+#include <argp.h>
 #include <cairo.h>
 #include "include.h"
+const char *argp_program_version = "The Christoffen Corporation Logo Generator v2.0.1";
+const char *argp_program_bug_address = "M. Gage Morgan <gage@christoffen.com>";
+static char doc[] = "Generates all of the logo, or just some.";
 
-int main() {
-	outlined_triangle(421, 410, 10, 10, 2.3, 28.1, 14.3, 3.0, 19.8, 17.6, 2.3, 28.1, "triangle1-outline.png");
-	outlined_triangle(421, 410, 10, 10, 10.0, 11.8, 27.5, 4.4, 37.7, 10.2, 10.0, 11.8, "triangle2-outline.png");
-	outlined_triangle(421, 410, 10, 10, 30.5, 2.6, 36.3, 19.0, 25.0, 12.6, 30.5, 2.6, "triangle3-outline.png");
-	outlined_triangle(421, 410, 10, 10, 29.0, 14.7, 37.6, 13.0, 30.4, 22.2, 29.0, 14.7, "triangle4-outline.png");
-	outlined_triangle(421, 410, 10, 10, 7.0, 18.4, 24.4, 31.4, 14.8, 37.5, 7.0, 18.4, "triangle5-outline.png");
-	outlined_triangle(421, 410, 10, 10, 12.8, 33.0, 19.5, 26.0, 33.3, 35.6, 12.8, 33.0, "triangle6-outline.png");
-	outlined_triangle(421, 410, 10, 10, 25.2, 32.6, 30.4, 27.0, 31.1, 38.0, 25.2, 32.6, "triangle7-outline.png");
+static int parse_opt (int key, char *arg, struct argp_state *state) {
+ switch (key) {
+ case 'C': 
+ colored_nologo(); 
+ break;
+ case 'c':
+ colored_all();
+ break;
+ case 'O':
+ outlined_nologo();
+ break;
+ case 'o':
+ outlined_all();
+ break;
+ case 'F':
+ frankenlogos();
+ break;
+ case 'a': 
+ all_imgs();
+ }
+ return 0;
+}
 
-	colored_triangle(421, 410, 0, 0.79, 0.83, 0.8, 143, 30, 198, 176, 23, 281, "triangle1-color.png");
-	colored_triangle(421, 410, 0.60, 0.85, 0.91, 0.8, 275, 44, 377, 102, 100, 118, "triangle2-color.png");
-	colored_triangle(421, 410, 0.28, 0.42, 0.61, 0.8, 363, 190, 250, 126, 305, 26, "triangle3-color.png");
-	colored_triangle(421, 410, 0, 0.63, 0.90, 0.8, 376, 130, 304, 222, 290, 147, "triangle4-color.png");
-	colored_triangle(421, 410, 0.60, 0.85, 0.91, 0.8, 244, 314, 148, 375, 70, 184, "triangle5-color.png");
-	colored_triangle(421, 410, 0.28, 0.42, 0.61, 0.8, 195, 260, 333, 356, 128, 330, "triangle6-color.png");
-	colored_triangle(421, 410, 0, 0.63, 0.90, 0.8, 304, 270, 311, 380, 252, 326, "triangle7-color.png");
-
-	join_triangles("triangle1-outline.png", "triangle2-outline.png", "triangle3-outline.png", "triangle4-outline.png", "triangle5-outline.png", "triangle6-outline.png", "triangle7-outline.png", "crstfn-outline.png");
-	join_triangles("triangle1-color.png", "triangle2-color.png", "triangle3-color.png", "triangle4-color.png", "triangle5-color.png", "triangle6-color.png", "triangle7-color.png", "crstfn-color.png");
-	return 0;
+int main (int argc, char **argv) {
+ struct argp_option options[] =
+ {
+ { "colored-no-logo", 'C', 0, 0, "Generate all colored triangles, except for the logo"},
+ { "colored-all", 'c', 0, 0, "Generate all colored triangles, and the logo"},
+ { "outlined-no-logo", 'O', 0, 0, "Generate all outlined triangles, except for the logo"},
+ { "outlined-all", 'o', 0, 0, "Generate all outlined triangles, and the logo"}, 
+ { "frankenlogos", 'F', 0, 0, "Generate the Frankenlogos (don't ask; just do)"}, 
+ { "all-images", 'a', 0, 0, "Generate all images: Outlines, Colors, and Logos."},
+ { 0 }
+ };
+ struct argp argp = { options, parse_opt, 0, 0 };
+ return argp_parse (&argp, argc, argv, 0, 0, 0); 
 }
