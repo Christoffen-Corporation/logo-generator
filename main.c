be35497ea21bda0ebf6edbe14a176fe024d07047
M. Gage Morgan <gagemorgan@outlook.com>
2016-06-09 13:18:13 -0400

    New GUI interface, no time to document fully unfortunately

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <argp.h>
-#include <cairo.h>
+#include <gtk/gtk.h>
 #include "include.h"
+
 const char *argp_program_version = "The Christoffen Corporation Logo Generator v2.0.1";
 const char *argp_program_bug_address = "M. Gage Morgan <gage@christoffen.com>";
 static char doc[] = "Generates all of the logo, or just some.";
@@ -27,10 +28,103 @@ static int parse_opt (int key, char *arg, struct argp_state *state) {
  return 0;
 }
 
-int main (int argc, char **argv) {
+static void
+frankenlogo (GtkWidget *widget,
+             gpointer   data)
+{
+  frankenlogos();
+}
+
+static void
+color_no_logo (GtkWidget *widget,
+             gpointer   data)
+{
+  colored_nologo();
+}
+
+static void
+all (GtkWidget *widget,
+             gpointer   data)
+{
+  all_imgs();
+}
+
+static void
+outline_no_logo (GtkWidget *widget,
+             gpointer   data)
+{
+  outlined_nologo();
+}
+
+static void
+outline_all (GtkWidget *widget,
+             gpointer   data)
+{
+  outlined_all();
+}
+
+static void
+color_all (GtkWidget *widget,
+             gpointer   data)
+{
+  colored_all();
+}
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *button_box;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Christoffen Corporation Logo Generator");
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 500);
+
+  button_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
+  gtk_container_add (GTK_CONTAINER (window), button_box);
+  
+  button = gtk_button_new_with_label ("Frankenlogos");
+  g_signal_connect (button, "clicked", G_CALLBACK (frankenlogo), NULL);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  button = gtk_button_new_with_label ("Colored (no logo)");
+  g_signal_connect (button, "clicked", G_CALLBACK (color_no_logo), NULL);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  button = gtk_button_new_with_label ("All");
+  g_signal_connect (button, "clicked", G_CALLBACK (all), NULL);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  button = gtk_button_new_with_label ("Outlined (no logo)");
+  g_signal_connect (button, "clicked", G_CALLBACK (outline_no_logo), NULL);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  button = gtk_button_new_with_label ("Outlined (all)");
+  g_signal_connect (button, "clicked", G_CALLBACK (outline_all), NULL);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  gtk_widget_show_all (window);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
 if (argc == 1) {
-all_imgs();
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.christoffen-logo-generator", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+
 } else {
+
  struct argp_option options[] =
  {
  { "colored-no-logo", 'C', 0, 0, "Generate all colored triangles, except for the logo\n"},
